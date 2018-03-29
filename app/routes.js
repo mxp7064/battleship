62fb3ef611a64a10ca3544d460381764cfe42efe
mxp7064 <mxp7064@g.rit.edu>
2018-03-30 00:14:12 +0200

    add mLab conn string

--- app/routes.js
+++ app/routes.js
@@ -6,7 +6,8 @@ const { matchedData, sanitize } = require('express-validator/filter');
 
 module.exports = function (jwt, router) {
     var mongoose = require("mongoose");
-    mongoose.connect("mongodb://localhost/Battleship");
+    //mongoose.connect("mongodb://localhost/Battleship");
+    mongoose.connect("mongodb://marko:thebattleshipdream123@ds227939.mlab.com:27939/battleship");
 
     var db = mongoose.connection;
 
